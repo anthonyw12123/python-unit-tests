ae4ad2635c5570484beb9b63dc76caa0c419ec07
Matthew Laney <matthewslaney@gmail.com>
2020-11-25 23:20:46 -0800

    Created List model Added FK to List on Item model Extended unit tests to create lists Verified lists could be saved and retrieved from database Modified new_list view to create List and add list FK when creating Items Completed chapter 7.9

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -23,15 +23,23 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
 
-class ItemModelTests(TestCase):
+class ListAndItemModelTests(TestCase):
     def test_saving_and_retrieving_items(self):
+        test_list = List()
+        test_list.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = test_list
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = test_list
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, test_list)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -39,7 +47,9 @@ class ItemModelTests(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, test_list)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, test_list)
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
@@ -47,8 +57,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='Item1')
-        Item.objects.create(text='Item42')
+        test_list = List.objects.create()
+        Item.objects.create(text='Item1', list=test_list)
+        Item.objects.create(text='Item42', list=test_list)
 
         response = self.client.get('/lists/one-list-to-rule-them-all')
 

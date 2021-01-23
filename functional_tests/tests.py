fbf81cdb613740d0aaa902f1e11535dfa6f2d103
Anthony Woody <powderedegs@gmail.com>
2021-01-23 15:17:09 -0800

    Finished 8.1. Implemented initial functional test for upcoming aesthetic changes.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -115,3 +115,20 @@ class NewVisitorTest(LiveServerTestCase):
         '''
 
         # Francis and Edith sleep together
+
+    # Aesthetics Testing
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        # She notices the inpux box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('testing')
+        inputbox.send_keys('Keys.ENTER')
+        self.verify_row_against_table('in', '1: testing')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
